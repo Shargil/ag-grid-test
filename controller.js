1f8bce920aa0e916879f0f9d606d032f63bd67e2
Shargil <yamshargil@gmail.com>
2018-02-28 09:17:33 +0200

    Add new big table (without secondery)

--- controller.js
+++ controller.js
@@ -11,9 +11,30 @@ app.controller("myCtrl", function($scope) {
         {ID: "date", headerName: "תאריך", field: "date", type : "dateColumn",  filter: "agDateColumnFilter"}, // , לבדוק פורמטר, תאריך
         {ID: "isEmpty", headerName: "שדרה ריקה", field: "isEmpty", width: 100, type: ["nonEditableColumn"], cellRenderer: 'isConvoyEmptyRenderer'}, //אייקון
         {ID: "status", headerName: "סטטוס שדרה", children : [
-            {headerName: "בהעמסה", field : "status_loading", suppressFilter: true, width: 90}, 
-            {headerName: "בפריקה", field : "status_unloading", suppressFilter: true,  width: 90}, 
-            {headerName: "בתנועה", field : "status_movement", suppressFilter: true,  width: 90} 
+            {headerName: "בהעמסה", field : "status_loading", suppressFilter: true, width: 90, cellStyle : function(params){
+                if (params.value == "כן") {
+                    //mark police cells as red
+                    return {backgroundColor: '#bde5c8'};
+                } else if(params.value){
+                    return {backgroundColor: '#dbdbdb'};
+                }
+            }}, 
+            {headerName: "בפריקה", field : "status_unloading", suppressFilter: true,  width: 90, cellStyle : function(params){
+                if (params.value == "כן") {
+                    //mark police cells as red
+                    return {backgroundColor: '#bde5c8'};
+                } else {
+                    return {backgroundColor: '#dbdbdb'};
+                }
+            }}, 
+            {headerName: "בתנועה", field : "status_movement", suppressFilter: true,  width: 90, cellStyle : function(params){
+                if (params.value == "כן") {
+                    //mark police cells as red
+                    return {backgroundColor: '#bde5c8'};
+                } else {
+                    return {backgroundColor: '#dbdbdb'};
+                }
+            }} 
         ]}, // איחוד כותרת בעמודות
         {ID: "comments", headerName: "הערות", field: "comments"}, // לוודא עריכה
         {ID: "comments2", headerName: "הערות אנגלית", field: "comments2"}, // לוודא עריכה
@@ -33,23 +54,24 @@ app.controller("myCtrl", function($scope) {
         }
     }
 
-    var rowData = [
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל1"},
-        
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל2"},
-        
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל3"},
-        
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "4ים שרגיל"},
-        
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "5ים שרגיל"},
-        
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "6ים שרגיל"},
-        
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "7ים שרגיל"},
-        
-        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: true, status_unloading: false, status_movement: false, comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל8"}
-    ];
+    var rowData = MOCK_DATA;
+//        [
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל1"},
+//        
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל2"},
+//        
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל3"},
+//        
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "4ים שרגיל"},
+//        
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "5ים שרגיל"},
+//        
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "6ים שרגיל"},
+//        
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "7ים שרגיל"},
+//        
+//        {convoyName: "השדרה הטובה בעולם", date: new Date(), isEmpty: true, status_loading: "כן", status_unloading: "לא", status_movement: "לא", comments: "תערוך אותי!", comments2: "Edit me!", convoyNum: 22, isCool: "מגניב", ituran: "1234567", driverName: "ים שרגיל8"}
+//    ];
 
     // a default column definition with properties that get applied to every column
     var defaultColDef = {
@@ -145,4 +167,9 @@ app.controller("myCtrl", function($scope) {
 
         $scope.closeColumnsOrganizer();
     };
+    
+    //------------------------------------- Tool Bar -------------------------------------//
+    $scope.deleteRows = function() {
+        this.mainGridOptions.api.updateRowData({remove : this.mainGridOptions.api.getSelectedRows()})
+    }
 });
